cb6e4f40ac2085a1f56f22db7878716c1339d4c6
Baran Usluel <me@baranusluel.com>
2017-11-30 15:10:04 -0500

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 2048-3d-matlab
+This is a 3D version of the 2048 game, implemented in MATLAB. It was created as a project for CS1371.
---
+++ tileGenerator.m
@@ -0,0 +1,5 @@
+%
+%updatedArr = tileGenerator(arr)
+%takes in a 4x4 array of doubles
+%randomly generate a 2 or 4 on the board in place of a 0
+%returns the input if the input array is not a 4X4 array of doubles
